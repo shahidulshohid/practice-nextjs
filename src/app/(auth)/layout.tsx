94429709341291137fdb16be56298671717452fe
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-06-01 14:51:39 +0600

    navbar practic is done

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,17 +1,75 @@
+// "use client";
+// import Link from "next/link";
+// import { usePathname } from "next/navigation";
+// import  "./style.css"
+
+// const navLinks = [
+//   { name: "register", href: "/register" },
+//   { name: "login", href: "/login" },
+//   { name: "forgot password", href: "/forgot-password" },
+// ];
+
+// export default function AuthLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   const pathName = usePathname();
+
+//   return (
+//     <html lang="en">
+//       <body>
+//         <nav>
+//           {navLinks.map((link) => {
+//             const isActive =
+//               pathName === link.href ||
+//               (pathName.startsWith(link.href) && link.href !== "/");
+
+//             return (
+//               <Link
+//                 href={link.href}
+//                 key={link.name}
+//                 className={isActive ? "font-bold mr-4 active" : "text-blue-500 mr-4"}
+//               >
+//                 {link.name}
+//               </Link>
+//             );
+//           })}
+//         </nav>
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+
+
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import "./style.css"
+const navLinks = [
+  {name: "register", href: "/register"},
+  {name: "login", href: "/login"},
+  {name: "forgot password", href: "/forgot-password"},
+]
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathName = usePathname()
   return (
     <html lang="en">
       <body>
+        {
+          navLinks.map((link) => {
+            const isActive = 
+              pathName === link.href || (pathName.startsWith(link.href) && pathName !== '/')
+            return (
+              <Link className={
+                isActive ? "active" : " "
+              } href={link.href} key={link.name}>{link.name}</Link>
+            )
+          })
+        }
         {children}
-        <footer 
-        style={{
-            backgroundColor: "ghostwhite",
-            padding: "1rem"
-        }}
-        >
-            <h1>Footer</h1>
-        </footer>
       </body>
     </html>
   );
